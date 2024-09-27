55c4df14069537df815db9edcb0b1d5ece72e3e2
RaminduA <raminduanjana22171@gmail.com>
2024-09-28 00:04:23 +0530

    implemented builder pattern

--- Creational Patterns/Builder/BuilderMain.java
+++ Creational Patterns/Builder/BuilderMain.java
@@ -1,8 +1,13 @@
 public class BuilderMain {
     public static void main(String[] args) {
-        Builder builder1 = new Builder1();
+        Builder builder1 = new Builder1().buildStep1().buildStep3();
         Product product1 = builder1.getResult();
-        Builder builder2 = new Builder2();
+
+        Builder builder2 = new Builder2().buildStep2().buildStep3();
         Product product2 = builder2.getResult();
+
+        builder1.reset();
+        builder1.buildStep1().buildStep2().buildStep3();
+        Product product3 = builder1.getResult();
     }
 }
